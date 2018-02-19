65d63ac1bb67187c41cab5b140dcd2358a110963
Aaron Fay <aaron.j.fay@gmail.com>
2018-02-19 15:26:12 -0700

    move core files into task folder

---
+++ run-suite-task/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tsc
+
+export INPUT_SUITEID='59fa3ed2e6fa2c7eb595a8e5'
+export INPUT_APIKEY='af923ddcd57dcaca093c7369f3768abf631bb379'
+export INPUT_STARTURL='http://foobar.com'
+
+node index.js
+
+
